901408e50a7a2469d341d7062a584eaba7e366c7
JoaquinZabala <joacozabala21@gmail.com>
2022-10-26 09:20:09 -0300

    Javascript leccion02
    Agregamos nueva sintaxis de js y java , tambien agregamos nuevo contenido a la concatenacion de cadenas

--- javascript/leccion02/02-01ConcatenacionDeCadenas.js
+++ javascript/leccion02/02-01ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido;
 console.log(nombre);
+
+//hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; //una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de una misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z)
+
+let $1num = 31; //No tuilizar numero para iniciar el nombre de una variable
+let rompiendo = "rompe"; //No utilizar palabras reservadas para variables break
+
+console.log($1num)
+console.log(rompiendo)
