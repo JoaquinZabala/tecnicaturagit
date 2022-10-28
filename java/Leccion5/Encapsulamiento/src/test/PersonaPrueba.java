b71b9ff3b9b1ba5faa3ad331722202dbb560c9b1
JoaquinZabala <joacozabala21@gmail.com>
2022-10-28 19:17:09 -0300

    Leccion 5 java
    Leccion 5 java Personaprueba - Encapsulamiento

--- java/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
+++ java/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
@@ -6,6 +6,7 @@ import Dominio.Persona;
 public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Osvaldo", 57000, false);
+        System.out.println("persona1 = " + persona1);
         System.out.println("persona1 con su nombre modificado: "+persona1.getNombre());
 
         //Modificar a traves de los metodos
@@ -15,6 +16,9 @@ public class PersonaPrueba {
         System.out.println("el nombre para persona1 es: "+persona1.getNombre());
         System.out.println("el sueldo de persona1 es: "+persona1.getSueldo());
         System.out.println("persona1 para obtener el booleano: "+persona1.getEliminado());
-        
+     //Tarea: Crear otro objeto tipo Persona, asignar valores de manera inicial
+     //Y imprimir, luego modificar sus valores y volver a imprimir
+        //System.out.println("persona1: "+persona1.toString());
+        System.out.println("persona1 = " + persona1);//Llama automaticamente al tostring
     }
 }
