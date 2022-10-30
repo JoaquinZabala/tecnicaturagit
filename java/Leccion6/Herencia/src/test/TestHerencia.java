c27bcccdf7bba3fe718b9bf764cddeec2f8f5928
JoaquinZabala <joacozabala21@gmail.com>
2022-10-30 19:58:45 -0300

    Java leccion 6
    leccion 6 de java, Herencia, Persona, Empleado

---
+++ java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import domain.Empleado;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Ariel", 67000.0);
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
