8fe45fdd65316ed6f2b022ee1f77378a5ae3afd5
JoaquinZabala <joacozabala21@gmail.com>
2022-11-12 18:33:35 -0300

    Nuevos archivos java
    Nuevos archivos java proyecto pseint a java

---
+++ java/ProyectoPseint/ProyectoPseint/src/proyectopseint/Sysacad.java
@@ -0,0 +1,42 @@
+
+package proyectopseint;
+
+public class Sysacad {
+    public static void main(String[] args) {
+        /*
+        Proyecto Grupo Programacion UTN
+        Proyecto Sysacad:
+        Este proyecto trata de un programa el cual puede acceder a la informacion
+        de un alumno por medio de un numero de legajo y contraseña
+        Podremos acceder a informacion tal como: materias del plan, examenes,
+        correlatividades,etc.
+        Este programa esta pensado para una escuela/facultad, para permitir que
+        un alumno pueda revisar su estado academico por medio de un programa
+        
+        */
+        System.out.println("""
+                                                           "Universidad Tecnologica Nacional"
+                                                            " Facultad Regional San Rafael "
+                                                                              
+                                                              " Sistema Academico SYSCADA "
+                                                            " Modulo de autogestion alumnos "
+                                                                              
+                            """);
+        
+        
+        
+        System.out.println("""
+                           \t "   A\u00f1o     |   Dic.    |         Materia                  | Se cursa | Se rinde  |";
+                           \t "    1      |    1c     |Matem\u00e1tica                        |   Si     |    Si     |";
+                           \t "    1      |    1c     |Programaci\u00f3n 1                    |   Si     |    Si     |";
+                           \t "    1      |    1c     |Sistemas de Pocesamiento de Datos |   Si     |    Si     |";
+                           \t "    1      |    1c     |Ingles 1                          |   Si     |    Si     |";
+                           \t "    1      |    1c     |Laboratorio de computaci\u00f3n 1      |   Si     |    Si     |";
+                           \t "    1      |    2c     |Programaci\u00f3n 2                    |   Si     |    Si     |";
+                           \t "    1      |    2c     |Arquitectura y Sistemas Operativos|   Si     |    Si     |";
+                           \t "    1      |    2c     |Estad\u00edstica                       |   Si     |    Si     |";
+                           \t "    1      |    2c     |Ingles 2                          |   Si     |    Si     |";
+                           \t "    1      |    2c     |Laboratorio de computaci\u00f3n 2      |   Si     |    Si     |";
+                           \t "    1      |    2c     |Metodolog\u00eda de la investigaci\u00f3n   |   Si     |    Si     |";"""); 
+    }
+}
