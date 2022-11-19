c9f925f924e52e25299f9116a9b668a3941233b0
JoaquinZabala <joacozabala21@gmail.com>
2022-11-19 10:57:17 -0300

    Leccion 10 / constantes
    Leccion 10 de python y constantes

---
+++ python/Leccion10/TestConstantes.py
@@ -0,0 +1,8 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+# Mi_CONSTANTE = "Aqui esta el nuevo calor para esta constante"
+# print(MI_CONSTANTE) Esto no se debe hacer
+
+print(Mate)
